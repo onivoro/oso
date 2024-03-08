0c783c8bd83edac2ad00700529483b9473d33053
icedlee337 <lee.e.norris@gmail.com>
2024-03-08 00:31:25 -0600

    chore: no more iteration over Env enum

--- src/lib/config/env.function.ts
+++ src/lib/config/env.function.ts
@@ -1,9 +1,16 @@
 import { Env } from "./env.enum";
 
 export function env(): { [key in Env]: any } {
-    return Object.values(Env).reduce((_, key) => {
-        _[key] = process.env[key];
+    // return Object.values(Env).reduce((_, key) => {
+    //     _[key] = process.env[key];
 
-        return _;
-    }, {} as any);
+    //     return _;
+    // }, {} as any);
+
+    return {
+        AWS_ACCESS_KEY_ID: process.env['AWS_ACCESS_KEY_ID'],
+        AWS_BUCKET: process.env['AWS_BUCKET'],
+        AWS_REGION: process.env['AWS_REGION'],
+        AWS_SECRET_ACCESS_KEY: process.env['AWS_SECRET_ACCESS_KEY'],
+    };
 }
